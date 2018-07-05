192b1c56ea5532b0c329c98ac35fe867e75f5631
jon <kawadu1221@gmail.com>
2018-07-06 00:14:14 +0900

    create signin form

--- app/src/components/Signin.vue
+++ app/src/components/Signin.vue
@@ -8,14 +8,38 @@
     </div>
   </nav>
 
-  Signin
-  <div class="button">
-  <a class="waves-effect waves-light btn-large">Signin</a>
-  <a class="waves-effect waves-light btn-large">Signup</a>
+  <div class="row">
+    <form class="col s12">
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="name" type="text" class="validate">
+          <label for="username">Name</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="email" type="email" class="validate">
+          <label for="email">Email</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="password" type="password" class="validate">
+          <label for="password">Password</label>
+        </div>
+      </div>
+      <div class="row">
+           <div class="input-field col s12">
+            <button class="btn cyan waves-effect waves-light right" type="submit" name="action">Submit
+              <i class="mdi-content-send right"></i>
+            </button>
+           </div>
+      </div>
+    </form>
   </div>
+
   </div>
 </template>
-
 
 <script>
 export default {
