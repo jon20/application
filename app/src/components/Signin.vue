befbc8514091f78a3892f1943bf12d1355b82f5c
jon <kawadu1221@gmail.com>
2018-07-07 01:22:12 +0900

    fix post form

--- app/src/components/Signin.vue
+++ app/src/components/Signin.vue
@@ -12,25 +12,25 @@
     <form class="col s12">
       <div class="row">
         <div class="input-field col s12">
-          <input id="name" type="text" class="validate">
+          <input id="name" type="text" v-model="name" class="validate">
           <label for="username">Name</label>
         </div>
       </div>
       <div class="row">
         <div class="input-field col s12">
-          <input id="email" type="email" class="validate">
+          <input id="email" type="email" v-model="email" class="validate">
           <label for="email">Email</label>
         </div>
       </div>
       <div class="row">
         <div class="input-field col s12">
-          <input id="password" type="password" class="validate">
+          <input id="password" type="password" v-model="password" class="validate">
           <label for="password">Password</label>
         </div>
       </div>
       <div class="row">
            <div class="input-field col s12">
-            <button class="btn cyan waves-effect waves-light right" type="submit" name="action">Submit
+            <button class="btn cyan waves-effect waves-light right" type="submit" v-on:click="signIn">Submit
               <i class="mdi-content-send right"></i>
             </button>
            </div>
@@ -42,11 +42,25 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'signin',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      name: '',
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    signIn: function() {
+      let params = new URLSearchParams();
+      params.append('name', this.name);
+      params.append('email', this.email);
+      params.append('password', this.password);
+      axios.post('http://localhost:8000/auth', params).then(res =>
+        console.log(res.data)
+      )
     }
   }
 }
